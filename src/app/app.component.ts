2d3ca8d3b753cca3b0da2240a7d7112530ad69dc
Mariiopl <157810818+Mariiopl@users.noreply.github.com>
2025-03-24 09:22:29 +0100

    Comunicacion Padre a Hijo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { ListadoComponent } from './listado/listado.component';
 import { DetalleComponent } from "./detalle/detalle.component";
 import { BanderaComponent } from "./bandera/bandera.component";
+import { MensajePadreComponent } from "./mensaje-padre/mensaje-padre.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DetalleComponent, ListadoComponent, BanderaComponent],
+  imports: [RouterOutlet, DetalleComponent, ListadoComponent, BanderaComponent, MensajePadreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/mensaje-padre/mensaje-padre.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-mensaje-padre',
+  imports: [],
+  templateUrl: './mensaje-padre.component.html',
+  styleUrl: './mensaje-padre.component.css'
+})
+export class MensajePadreComponent {
+@Input() childMessage: string | undefined;
+}
